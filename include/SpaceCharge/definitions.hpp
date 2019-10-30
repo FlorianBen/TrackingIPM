f3d0236cf03b87f932f6c93cfce88fadfba59cb9
Florian <f-benedetti@live.com>
2019-10-30 22:57:50 +0100

    Update definitions.

--- include/SpaceCharge/definitions.hpp
+++ include/SpaceCharge/definitions.hpp
@@ -3,7 +3,8 @@
 
 #include <boost/math/constants/constants.hpp>
 
-namespace SpaceCharge::cst {
+namespace SpaceCharge {
+namespace cst {
 constexpr double pi = boost::math::constants::pi<double>();
 
 const double g = 9.8;
@@ -12,7 +13,7 @@ const double k = 1.380649e-23;
 const double Na = 6.02214076e23;
 const double R = Na * k;
 
-enum lfactor { speed, beta, gamma };
+enum lfactor { speed, beta, gamma, ec };
 const double hp = 6.62607015e-34;
 const double hpb = hp / (2 * pi);
 const double sol = 2.99792458e8;
@@ -20,9 +21,10 @@ const double e = 1.6021766208e-19;
 const double mu0 = 4 * pi * 1e-7;
 const double eps0 = 1 / (mu0 * sol * sol);
 
-//TODO: Add some constant mass value 
-const double mproton =0;
-
-} // namespace SpaceCharge::cst
-
+// TODO: Add some constant mass value
+const double mproton = 1.672649e-27;
+const double melectron = 9.109e-31;
+const double mneutron = 1.67493e-27;
+} // namespace cst
+} // namespace SpaceCharge
 #endif
