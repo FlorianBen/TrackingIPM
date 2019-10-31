b88a96f52ec427902be8e1405fd4931922e34d68
Florian <f-benedetti@live.com>
2019-10-31 21:49:42 +0100

    Add field classes.

---
+++ include/SpaceCharge/fields.hpp
@@ -0,0 +1,15 @@
+#ifndef FIELDS_HPP
+#define FIELDS_HPP
+
+#include "bunch.hpp"
+
+namespace SpaceCharge {
+template <class T> class Field {
+private:
+    
+public:
+  Field(Bunch<T> bunch);
+};
+}; // namespace SpaceCharge
+
+#endif
