fa96935a4d7e185d1c31de1698fb9d8e6bab890d
Florian <f-benedetti@live.com>
2019-10-31 21:50:12 +0100

    Implementation of Bunch.

--- include/SpaceCharge/bunch.hpp
+++ include/SpaceCharge/bunch.hpp
@@ -3,13 +3,15 @@
 
 #include <string>
 
+#include <Eigen/Dense>
+
 #include "SpaceCharge/definitions.hpp"
 #include "SpaceCharge/particle.hpp"
 
 namespace SpaceCharge {
 
 template <class T> class Bunch {
-private:
+protected:
   Particle<T> particle;
   T Qb;
   T ib;
@@ -18,17 +20,35 @@ private:
   T dt;
 
   void check_particle_def(std::string pdg_name);
+  void updateBunch();
 
 public:
-  Bunch();
-  Bunch(std::string pdg_name, T ib, T dt);
+  // Bunch(std::string pdg_name, T ib, T dt);
   Bunch(Particle<T> particle, T ib, T dt);
   Bunch(int p_charge, T pmass, cst::lfactor factor, T lfactor_v, T ib, T dt);
+
+  Particle<T> getParticle() const;
+  T getCurrent() const;
+  T getCharges() const;
+  int getNbParticles() const;
+  T getFreqRF() const;
+  T getBunchPeriod() const;
 
   // virtual T PotentialAt() const = 0;
 };
 
-template <class T> class GaussianBunch : public Bunch<T> {};
+template <class T> class GaussianBunch : public Bunch<T> {
+  typedef Eigen::Matrix<T, 4, 1> quadv;
+
+protected:
+  quadv sigma;
+  quadv sigma_;
+
+public:
+  GaussianBunch(Particle<T> particle, T ib, T dt, quadv sigma);
+  GaussianBunch(int p_charge, T pmass, cst::lfactor factor, T lfactor_v, T ib,
+                T dt, quadv sigma);
+};
 
 template <class T> class FEMBunch : public Bunch<T> {};
 } // namespace SpaceCharge
--- src/bunch.cpp
+++ src/bunch.cpp
@@ -2,15 +2,59 @@
 
 namespace SpaceCharge {
 
-template <class T> Bunch<T>::Bunch(){};
+// template <class T>
+// Bunch<T>::Bunch(std::string pdg_name, T ib, T dt) : ib(ib), dt(dt) {
+//   check_particle_def(pdg_name);
+// };
 
 template <class T>
-Bunch<T>::Bunch(std::string pdg_name, T ib, T dt)
-    : ib(ib), dt(dt){
-        check_particle_def(pdg_name);
-              };
+Bunch<T>::Bunch(Particle<T> particle, T ib, T dt)
+    : particle(particle), ib(ib), dt(dt) {
+  updateBunch();
+};
 
-//template class Bunch<double>;
-//template class Bunch<float>;
+template <class T>
+Bunch<T>::Bunch(int p_charge, T pmass, cst::lfactor factor, T lfactor_v, T ib,
+                T dt)
+    : particle("particle", p_charge, pmass, factor, lfactor_v), ib(ib), dt(dt) {
+  updateBunch();
+};
+
+template <class T>
+void Bunch<T>::check_particle_def(std::string pdg_name){
+
+};
+
+template <class T> void Bunch<T>::updateBunch() {
+  Qb = ib * dt;
+  pb = Qb / cst::e;
+  rf_freq = 1.0 / dt;
+};
+
+template <class T> Particle<T> Bunch<T>::getParticle() const {
+  return Particle<T>(particle);
+};
+
+template <class T> T Bunch<T>::getCurrent() const { return ib; };
+
+template <class T> T Bunch<T>::getCharges() const { return Qb; };
+
+template <class T> int Bunch<T>::getNbParticles() const { return pb; };
+
+template <class T> T Bunch<T>::getFreqRF() const { return rf_freq; };
+
+template <class T> T Bunch<T>::getBunchPeriod() const { return dt; };
+
+template <class T>
+GaussianBunch<T>::GaussianBunch(Particle<T> particle, T ib, T dt, quadv sigma)
+    : Bunch<T>(particle, ib, dt), sigma(sigma){};
+
+template <class T>
+GaussianBunch<T>::GaussianBunch(int p_charge, T pmass, cst::lfactor factor,
+                                T lfactor_v, T ib, T dt, quadv sigma)
+    : Bunch<T>(p_charge, pmass, factor, lfactor_v), sigma(sigma){};
+
+template class Bunch<double>;
+template class Bunch<float>;
 
 } // namespace SpaceCharge
