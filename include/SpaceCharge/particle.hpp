36c4d71e1111892832fb5c80d9629f52dc580b4e
Florian <f-benedetti@live.com>
2019-11-01 14:19:44 +0100

    Update documentation.

--- include/SpaceCharge/particle.hpp
+++ include/SpaceCharge/particle.hpp
@@ -7,6 +7,11 @@
 
 namespace SpaceCharge {
 
+/**
+ * \class Particle particle.hpp
+ * \brief This class describes the particles that compose the bunche.
+ *
+ **/
 template <class T> class Particle {
 private:
   int pcharge;
@@ -24,19 +29,68 @@ private:
   void updateCBetaGamma();
 
 public:
-  Particle(std::string name, int p_charge, T pmass, cst::lfactor factor,
+  /**
+   * \brief Constructor.
+   * \param[in] name Particle name.
+   * \param[in] pcharge Particle charge.
+   * \param[in] pmass Particle mass.
+   * \param[in] factor Select speed, \f$\beta\f$, \f$\gamma\f$ or \f$E_{c}\f$
+   * factor.
+   * \param[in] lfactor_v Value of the factor.
+   **/
+  Particle(std::string name, int pcharge, T pmass, cst::lfactor factor,
            T lfactor_v);
 
+  /**
+   * \brief Get Particle mass.
+   * \return The value of the particle mass in kg.
+   **/
   T getMass() const;
+  /**
+   * \brief Get Particle charge.
+   * \return The value of the particle charge in C.
+   **/
   T getCharge() const;
+  /**
+   * \brief Get Particle kinetic energy.
+   * \return The value of the particle mass in J.
+   **/
   T getEc() const;
+  /**
+   * \brief Get Particle speed.
+   * \return The value of the particle mass in m/s.
+   **/
   T getSpeed() const;
+  /**
+   * \brief Get Particle \f$\beta\f$.
+   * \return The value of the particle \f$\beta\f$.
+   **/
   T getBeta() const;
+  /**
+   * \brief Get Particle \f$\gamma\f$.
+   * \return The value of the particle \f$\gamma\f$.
+   **/
   T getGamma() const;
 
+  /**
+   * \brief
+   * \param[in]
+   **/
   void setEc(const T Ec);
+  /**
+   * \brief
+   * \param[in]
+   **/
   void setSpeed(const T Speed);
+  /**
+   * \brief
+   * \param[in]
+   **/
   void setBeta(const T Beta);
+  /**
+   * \brief
+   * \param[in]
+   **/
   void setGamma(const T Gamma);
 };
 
--- include/SpaceCharge/units.hpp
+++ include/SpaceCharge/units.hpp
@@ -1,9 +1,21 @@
+/**
+ * \file units.hpp
+ * \brief This file contains the definitions of several pysical units.
+ * Each function of this project returns SI values, these values can be
+ * converted thanks to the definitions contained in this file.
+ **/
 #ifndef UNITS_HPP
 #define UNITS_HPP
 
 #include "definitions.hpp"
 
+/**
+ * \namespace SpaceCharge::uni
+ * \brief This namespace contains the definitions of pysical units nested in
+ * subnamespace.
+ **/
 namespace SpaceCharge::uni {
+
 const double femto = 1e-15;
 const double pico = 1e-12;
 const double nano = 1e-9;
@@ -15,7 +27,15 @@ const double giga = 1e9;
 const double tera = 1e12;
 const double peta = 1e15;
 
+/**
+ * \namespace SpaceCharge::uni::t
+ * \brief This namespace contains the definitions of pysical units
+ * about time information.
+ **/
 namespace t {
+/**
+ * \brief Time base SI unit: second.
+ **/
 const double s = 1.0;
 const double fs = s * femto;
 const double ps = s * pico;
@@ -28,9 +48,32 @@ const double Gs = s * giga;
 const double Ts = s * tera;
 const double Ps = s * peta;
 
+/**
+ * \brief Frequency base unit: Hertz.
+ * \f$\frac{1}{T}\f$
+ **/
+const double Hz = 1.0;
+const double fHz = Hz / femto;
+const double pHz = Hz / pico;
+const double nHz = Hz / nano;
+const double uHz = Hz / micro;
+const double mHz = Hz / milli;
+const double kHz = Hz / kilo;
+const double MHz = Hz / mega;
+const double GHz = Hz / giga;
+const double THz = Hz / tera;
+const double PHz = Hz / peta;
 } // namespace t
 
+/**
+ * \namespace SpaceCharge::uni::d
+ * \brief This namespace contains the definitions of pysical units
+ * about distance information.
+ **/
 namespace d {
+/**
+ * \brief Distance base SI unit: meter.
+ **/
 const double m = 1.0;
 const double fm = m * femto;
 const double pm = m * pico;
@@ -45,7 +88,15 @@ const double Pm = m * peta;
 
 } // namespace d
 
+/**
+ * \namespace SpaceCharge::uni::m
+ * \brief This namespace contains the definitions of pysical units
+ * about mass information.
+ **/
 namespace m {
+/**
+ * \brief Mass base SI unit: kilogramme.
+ **/
 const double g = 0.001;
 const double fg = g * femto;
 const double pg = g * pico;
@@ -59,7 +110,15 @@ const double Tg = g * tera;
 const double Pg = g * peta;
 } // namespace m
 
+/**
+ * \namespace SpaceCharge::uni::p
+ * \brief This namespace contains the definitions of pysical units
+ * about power information.
+ **/
 namespace p {
+/**
+ * \brief Power base SI unit: Joule.
+ **/
 const double J = 1.0;
 const double fJ = J * femto;
 const double pJ = J * pico;
@@ -72,6 +131,10 @@ const double GJ = J * giga;
 const double TJ = J * tera;
 const double PJ = J * peta;
 
+/**
+ * \brief eV: electronVolt.
+ * \f$E_{eV}=E_{Joule} \times 1.602176 \cdot 10^{-19}\f$
+ **/
 const double eV = 1.0 * cst::e;
 const double feV = eV * femto;
 const double peV = eV * pico;
