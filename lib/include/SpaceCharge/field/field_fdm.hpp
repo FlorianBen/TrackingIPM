8c4df716222eefe04066f3d23e739563e956b26f
Florian Benedetti <florian.benedetti@cea.fr>
2021-06-25 14:06:12 +0200

    Add FieldFDM header

---
+++ lib/include/SpaceCharge/field/field_fdm.hpp
@@ -0,0 +1,14 @@
+#ifndef FIELD_FDM_HPP
+#define FIELD_FDM_HPP
+
+namespace SpaceCharge {
+class FieldFDM {
+private:
+  /* data */
+public:
+  FieldFDM(/* args */);
+  ~FieldFDM();
+};
+} // namespace SpaceCharge
+
+#endif
