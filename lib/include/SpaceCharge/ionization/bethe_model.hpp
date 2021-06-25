28210945e90722569a919c3ecf3c244aede59f77
Florian Benedetti <florian.benedetti@cea.fr>
2021-06-25 14:06:35 +0200

    Add the Bethe Model class

---
+++ lib/include/SpaceCharge/ionization/bethe_model.hpp
@@ -0,0 +1,15 @@
+#ifndef BETHE_MODEL_HPP
+#define BETHE_MODEL_HPP
+
+namespace SpaceCharge {
+
+class BetheModel {
+private:
+  /* data */
+public:
+  BetheModel(/* args */);
+  ~BetheModel();
+};
+} // namespace SpaceCharge
+
+#endif
---
+++ lib/include/SpaceCharge/ionization/medium.hpp
@@ -0,0 +1,19 @@
+#ifndef MEDIUM_HPP
+#define MEDIUM_HPP
+
+namespace SpaceCharge {
+
+class Medium {
+private:
+  /* data */
+public:
+  Medium(/* args */);
+  ~Medium();
+};
+
+Medium::Medium(/* args */) {}
+
+Medium::~Medium() {}
+
+} // namespace SpaceCharge
+#endif
