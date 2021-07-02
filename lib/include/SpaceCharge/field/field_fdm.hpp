5ffb9738b145d8c0f49e08fa9219fa043fad6c80
Florian Benedetti <florian.benedetti@cea.fr>
2021-07-02 17:37:00 +0200

    Update FDM

--- lib/include/SpaceCharge/field/field_fdm.hpp
+++ lib/include/SpaceCharge/field/field_fdm.hpp
@@ -1,13 +1,34 @@
 #ifndef FIELD_FDM_HPP
 #define FIELD_FDM_HPP
 
+#include <Eigen/Eigen>
+
 namespace SpaceCharge {
 class FieldFDM {
+  typedef Eigen::SparseMatrix<double>
+      SpMat; // declares a column-major sparse matrix type of double
+  typedef Eigen::Triplet<double> Tri;
+
 private:
   /* data */
+  int nx;
+  int ny;
+  double dx;
+  double dy;
+  double t;
+
+  SpMat mat;
+
+
 public:
-  FieldFDM(/* args */);
+  FieldFDM(int nx, int ny, double dx, double dy);
   ~FieldFDM();
+
+  void init_boundary();
+  void init_rhs();
+  void init_matrix();
+  void solve();
+
 };
 } // namespace SpaceCharge
 
--- lib/src/field/field_fdm.cpp
+++ lib/src/field/field_fdm.cpp
@@ -1,8 +1,13 @@
 #include "SpaceCharge/field/field_fdm.hpp"
 
+#include "SpaceCharge/core/alogger.hpp"
+
 namespace SpaceCharge {
 
-FieldFDM::FieldFDM(/* args */) {}
+FieldFDM::FieldFDM(int nx, int ny, double dx, double dy)
+    : nx(nx), ny(ny), dx(dx), dy(dy), mat(nx, ny) {
+  Logger::GetLogger()->info("FieldFDM: Matrix ({},{}) created", nx, ny);
+}
 
 FieldFDM::~FieldFDM() {}
 
