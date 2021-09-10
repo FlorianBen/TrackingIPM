30727590bc1eafc26f569c067de8cab4bf496741
Florian Benedetti <florian.benedetti@cea.fr>
2021-09-10 17:13:05 +0200

    Move Interpolator class

---
+++ lib/include/SpaceCharge/core/interpolator.hpp
@@ -0,0 +1,43 @@
+#ifndef INTERPOLATOR_HPP
+#define INTERPOLATOR_HPP
+
+
+class Interpolator
+{
+private:
+    /* data */
+public:
+    Interpolator(/* args */);
+    ~Interpolator();
+};
+
+class InterpolatorNN
+{
+private:
+    /* data */
+public:
+    InterpolatorNN(/* args */);
+    ~InterpolatorNN();
+};
+
+class InterpolatorFEM
+{
+private:
+    /* data */
+public:
+    InterpolatorFEM(/* args */);
+    ~InterpolatorFEM();
+};
+
+class InterpolatorRegularGrid
+{
+private:
+    /* data */
+public:
+    InterpolatorRegularGrid(/* args */);
+    ~InterpolatorRegularGrid();
+};
+
+
+
+#endif
