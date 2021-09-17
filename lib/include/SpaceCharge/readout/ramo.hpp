3737fc4a98290962cd4892f04eba6824044bb767
Florian Benedetti <florian.benedetti@cea.fr>
2021-09-17 10:58:07 +0200

    First implementation of Ramo class

--- lib/include/SpaceCharge/readout/ramo.hpp
+++ lib/include/SpaceCharge/readout/ramo.hpp
@@ -8,17 +8,29 @@
 
 namespace SpaceCharge {
 
+/**
+ * @brief This class computes the signal induced on conductive strips according
+ * to the Ramo Shockley.
+ * 
+ * @tparam T
+ */
 template <typename T> class RamoComputation {
   typedef std::unique_ptr<FieldMap<T>> fmap;
   typedef std::unique_ptr<Track<T>> ftrack;
 
 public:
-    RamoComputation();
+  RamoComputation(fmap& map, ftrack& track);
+
+  ~RamoComputation();
 
 private:
+  int size;
   fmap map;
   ftrack track;
+  PointCloud<T> pointcloud_pos;
+  state_type2<T> tracjectory;
 
+  void computeTrajectory();
 
   void computeCurrent();
 
--- lib/src/readout/ramo.cpp
+++ lib/src/readout/ramo.cpp
@@ -1,2 +1,42 @@
 #include "SpaceCharge/readout/ramo.hpp"
+#include "SpaceCharge/core/alogger.hpp"
+#include "SpaceCharge/core/nanoflann.hpp"
+#include "SpaceCharge/core/point_cloud.hpp"
 
+namespace SpaceCharge {
+
+// TODO: Add more generic version.
+
+template <typename T>
+RamoComputation<T>::RamoComputation(fmap &map, ftrack &track)
+    : size(100), map(std::move(map)), track(std::move(track)) {
+  SC_INFO("RamoComputation:: Constructor");
+  computeTrajectory();
+}
+
+template <typename T> void RamoComputation<T>::computeTrajectory() {
+
+  auto pos = track->getPosVector().back();
+  auto vit = track->getSpeedVector().back();
+
+  auto d_max = map->sizes()(2) * map->steps()(2);
+  auto t_max = d_max / vit(1);
+
+  for (int y = 0; y < map->sizes()(2); y++) {
+    for (int x = 0; x < map->sizes()(1); x++) {
+      quadv<T> tmp_pos(0.0, x * map->steps()(1), y * map->steps()(2), 0.0);
+    }
+  }
+
+  for (auto i = 0; i < size; i++) {
+    quadv<T> tmp_pos{0.0, (1.0 * i / size) * t_max * vit(1),
+                     (1.0 * i / size) * t_max * vit(2), 0.0};
+    tracjectory.push_back(tmp_pos);
+  }
+}
+
+template <typename T> RamoComputation<T>::~RamoComputation() {}
+
+template class RamoComputation<double>;
+
+} // namespace SpaceCharge
