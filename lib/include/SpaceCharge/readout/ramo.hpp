c01dafca1f39b73e6596d9894637cff0ec71db7d
Florian Benedetti <florian.benedetti@cea.fr>
2021-09-17 15:58:45 +0200

    Working on ramo class.

--- lib/include/SpaceCharge/readout/ramo.hpp
+++ lib/include/SpaceCharge/readout/ramo.hpp
@@ -2,7 +2,9 @@
 #define RAMO_HPP
 
 #include <memory>
+#include <vector>
 
+#include "SpaceCharge/core/nanoflann.hpp"
 #include "SpaceCharge/field/field_map.hpp"
 #include "SpaceCharge/track/track.hpp"
 
@@ -11,30 +13,50 @@ namespace SpaceCharge {
 /**
  * @brief This class computes the signal induced on conductive strips according
  * to the Ramo Shockley.
- * 
+ *
  * @tparam T
  */
 template <typename T> class RamoComputation {
   typedef std::unique_ptr<FieldMap<T>> fmap;
   typedef std::unique_ptr<Track<T>> ftrack;
-
-public:
-  RamoComputation(fmap& map, ftrack& track);
-
-  ~RamoComputation();
+  typedef nanoflann::KDTreeSingleIndexAdaptor<
+      nanoflann::L2_Simple_Adaptor<T, PointCloud<T>>, PointCloud<T>, 4 /* dim */
+      >
+      kd_tree_nanoflann;
+  typedef std::vector<std::vector<T>> testssfd;
 
 private:
   int size;
   fmap map;
   ftrack track;
   PointCloud<T> pointcloud_pos;
+  kd_tree_nanoflann *index;
+  state_type2<int> coordinates;
+  state_type2<int> res_coordinates;
+
   state_type2<T> tracjectory;
+  std::vector<std::vector<T>> current;
+
+  void createPosCloud();
+
+  void createIndex(const int leaf_size = 20);
 
   void computeTrajectory();
 
   void computeCurrent();
 
   void computeStrips();
+
+public:
+  RamoComputation(fmap &map, ftrack &track);
+
+  ~RamoComputation();
+
+  const state_type2<int>& getCoordinates() const;
+
+  const state_type2<T>& getTrajectory() const;
+
+  const std::vector<std::vector<T>>& getCurrent() const;
 };
 } // namespace SpaceCharge
 #endif
--- lib/src/readout/ramo.cpp
+++ lib/src/readout/ramo.cpp
@@ -10,8 +10,24 @@ namespace SpaceCharge {
 template <typename T>
 RamoComputation<T>::RamoComputation(fmap &map, ftrack &track)
     : size(100), map(std::move(map)), track(std::move(track)) {
-  SC_INFO("RamoComputation:: Constructor");
+  createPosCloud();
   computeTrajectory();
+  computeCurrent();
+}
+
+template <typename T>
+const state_type2<T>& RamoComputation<T>::getTrajectory() const {
+  return tracjectory;
+}
+
+template <typename T>
+const state_type2<int>& RamoComputation<T>::getCoordinates() const {
+  return res_coordinates;
+}
+
+template <typename T>
+const std::vector<std::vector<T>>& RamoComputation<T>::getCurrent() const {
+  return current;
 }
 
 template <typename T> void RamoComputation<T>::computeTrajectory() {
@@ -22,16 +38,42 @@ template <typename T> void RamoComputation<T>::computeTrajectory() {
   auto d_max = map->sizes()(2) * map->steps()(2);
   auto t_max = d_max / vit(1);
 
-  for (int y = 0; y < map->sizes()(2); y++) {
-    for (int x = 0; x < map->sizes()(1); x++) {
-      quadv<T> tmp_pos(0.0, x * map->steps()(1), y * map->steps()(2), 0.0);
-    }
-  }
-
   for (auto i = 0; i < size; i++) {
     quadv<T> tmp_pos{0.0, (1.0 * i / size) * t_max * vit(1),
                      (1.0 * i / size) * t_max * vit(2), 0.0};
     tracjectory.push_back(tmp_pos);
+  }
+}
+
+template <typename T> void RamoComputation<T>::createPosCloud() {
+  for (int y = 0; y < map->sizes()(2); y++) {
+    for (int x = 0; x < map->sizes()(1); x++) {
+      quadv<T> tmp_pos(0.0, x * map->steps()(1), y * map->steps()(2), 0.0);
+      pointcloud_pos.pts.push_back(tmp_pos-map->offsets());
+      coordinates.push_back(quadv<int>(0, x, y, 0));
+    }
+  }
+  createIndex();
+}
+
+template <typename T>
+void RamoComputation<T>::createIndex(const int leaf_size) {
+  index = new kd_tree_nanoflann(
+      3, pointcloud_pos, nanoflann::KDTreeSingleIndexAdaptorParams(leaf_size));
+  index->buildIndex();
+}
+
+template <typename T> void RamoComputation<T>::computeCurrent() {
+  quadv<T> fieldv;
+  for (const auto &pos : tracjectory) {
+    T query_pt[4] = {pos[0], pos[1], pos[2], pos[3]};
+    size_t nb_result = size;
+    std::vector<size_t> ret_index(nb_result);
+    std::vector<T> out_dist_sqr(nb_result);
+    nb_result = index->knnSearch(&query_pt[0], nb_result, &ret_index[0],
+                                 &out_dist_sqr[0]);
+
+    res_coordinates.push_back(coordinates[ret_index[0]]);
   }
 }
 
