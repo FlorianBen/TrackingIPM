3035d60d347b43b1f4bae9fb43349980f53f51ee
Florian Benedetti <florian.benedetti@cea.fr>
2021-12-01 13:31:51 +0100

    Minor change of sample 4

--- samples/sample_004/main.cpp
+++ samples/sample_004/main.cpp
@@ -14,55 +14,60 @@ int main(int argc, char *argv[]) {
   /* Initialize logger */
   SpaceCharge::Logger::Init();
 
+  // Create output file
+  using namespace hdf5;
+  auto file = file::create("write_fieldmap.h5", file::AccessFlags::TRUNCATE);
+  auto root_group = file.root();
+  hdf5::property::LinkCreationList lcpl;
+  hdf5::property::DatasetCreationList dcpl;
+
   // Create a particle.
   SpaceCharge::Particle<double> part("proton", 1, SpaceCharge::cst::mproton,
-                                     SpaceCharge::cst::lfactor::beta, 0.999);
+                                     SpaceCharge::cst::lfactor::beta, 0.05);
 
   // Create a bunch
   auto bunch_current = 62.5 * SpaceCharge::uni::milli;
   auto bunch_period = 1.0 / (352.0 * SpaceCharge::uni::mega);
-  std::unique_ptr<SpaceCharge::Bunch<double>> bunch(
-      new SpaceCharge::GaussianBunch<double>(part, bunch_current, bunch_period,
-                                             SpaceCharge::cst::dir::z));
 
-  // Create a generic Field object
-  SpaceCharge::FieldSP<double> fields =
-      std::make_unique<SpaceCharge::BunchEMField<double>>();
-  static_cast<SpaceCharge::BunchEMField<double> *>(fields.get())
-      ->usePeriodicity(true);
-  static_cast<SpaceCharge::BunchEMField<double> *>(fields.get())
-      ->addBunch(std::move(bunch));
+  auto t_step = 10e-12;
+  for (auto i = 0; i < 8; i++) {
+    SC_INFO("Loop {}", i);
 
-  // Create a FieldMap
-  size_t nx = 200, ny = 200, nz = 200;
-  auto size_x = 50e-3, size_y = 50e-3, size_z = 50e-3;
-  SpaceCharge::quadv<size_t> size{0, nx, ny, nz};
-  SpaceCharge::quadv<double> step{0, size_x / nx, size_y / ny, size_z / nz};
-  SpaceCharge::quadv<double> offset{SpaceCharge::cst::sol * 0e-9, -size_x / 2,
-                                    -size_y / 2, -size_z / 2};
-  std::unique_ptr<SpaceCharge::FieldMap<double>> fieldmap =
-      std::make_unique<SpaceCharge::FieldMapInterpolate<double>>(size, step,
-                                                                 offset, 0e-12);
-  // Fill FieldMap with our bunch field and compute the EM field at all the
-  // given FieldMap coordinates
-  static_cast<SpaceCharge::FieldMapInterpolate<double> *>(fieldmap.get())
-      ->addField(fields);
-  static_cast<SpaceCharge::FieldMapInterpolate<double> *>(fieldmap.get())
-      ->computeField();
+    std::unique_ptr<SpaceCharge::Bunch<double>> bunch(
+        new SpaceCharge::GaussianBunch<double>(
+            part, bunch_current, bunch_period, SpaceCharge::cst::dir::z));
+    // Create a generic Field object
+    SpaceCharge::FieldSP<double> fields =
+        std::make_unique<SpaceCharge::BunchEMField<double>>();
+    static_cast<SpaceCharge::BunchEMField<double> *>(fields.get())
+        ->usePeriodicity(true);
+    static_cast<SpaceCharge::BunchEMField<double> *>(fields.get())
+        ->addBunch(std::move(bunch));
 
-  using namespace hdf5;
-  auto file = file::create("write_fieldmap.h5", file::AccessFlags::TRUNCATE);
-  auto root_group = file.root();
+    // Create a FieldMap
+    size_t nx = 200, ny = 200, nz = 200;
+    auto size_x = 50e-3, size_y = 50e-3, size_z = 50e-3;
+    SpaceCharge::quadv<size_t> size{0, nx, ny, nz};
+    SpaceCharge::quadv<double> step{0, size_x / nx, size_y / ny, size_z / nz};
+    SpaceCharge::quadv<double> offset{SpaceCharge::cst::sol * t_step * i -
+                                          (SpaceCharge::cst::sol * t_step * 4),
+                                      -size_x / 2, -size_y / 2, -size_z / 2};
+    std::unique_ptr<SpaceCharge::FieldMap<double>> fieldmap =
+        std::make_unique<SpaceCharge::FieldMapInterpolate<double>>(
+            size, step, offset, 0e-12);
+    // Fill FieldMap with our bunch field and compute the EM field at all the
+    // given FieldMap coordinates
+    static_cast<SpaceCharge::FieldMapInterpolate<double> *>(fieldmap.get())
+        ->addField(fields);
+    static_cast<SpaceCharge::FieldMapInterpolate<double> *>(fieldmap.get())
+        ->computeField();
 
-  hdf5::property::LinkCreationList lcpl;
-  hdf5::property::DatasetCreationList dcpl;
-  // dcpl.layout(hdf5::property::DatasetLayout::CHUNKED);
-  // dcpl.chunk(hdf5::Dimensions{256});
-
-  auto dset = root_group.create_dataset(
-      "Efield", datatype::create<SpaceCharge::FieldMap<double>>(),
-      dataspace::create(*fieldmap), dcpl, lcpl);
-  dset.write(*fieldmap);
+    auto dset = root_group.create_dataset(
+        "Efield" + std::to_string(i),
+        datatype::create<SpaceCharge::FieldMap<double>>(),
+        dataspace::create(*fieldmap), dcpl, lcpl);
+    dset.write(*fieldmap);
+  }
 
   return 0;
 }
--- samples/sample_004/script/read_fieldmap.py
+++ samples/sample_004/script/read_fieldmap.py
@@ -6,37 +6,39 @@ import h5py as h5
 
 def main():
     f = h5.File('build/write_fieldmap.h5', mode='r')
-    dset = f['Efield']
-    data_z = np.array(dset['z'])
-    data_x = np.array(dset['x'])
-    data_y = np.array(dset['y'])
+    for i in range(8):
+        dset = f['Efield{}'.format(i)]
+        data_z = np.array(dset['z'])
+        data_x = np.array(dset['x'])
+        data_y = np.array(dset['y'])
 
-    n = 100
+        n = 100
 
-    fig, axs = plt.subplots(3, 3)
-    axs[0,0].imshow(data_x[:, :, n])
-    axs[1,0].imshow(data_x[:, n, :])
-    axs[2,0].imshow(data_x[n, :, :])
-    axs[0,1].imshow(data_y[:, :, n])
-    axs[1,1].imshow(data_y[:, n, :])
-    axs[2,1].imshow(data_y[n, :, :])
-    axs[0,2].imshow(data_z[:, :, n])
-    axs[1,2].imshow(data_z[:, n, :])
-    axs[2,2].imshow(data_z[n, :, :])
+        fig, axs = plt.subplots(3, 3)
+        axs[0,0].imshow(data_x[:, :, n], clim=(-6e4, 6e4))
+        axs[1,0].imshow(data_x[:, n, :])
+        axs[2,0].imshow(data_x[n, :, :], clim=(-6e4, 6e4))
+        axs[0,1].imshow(data_y[:, :, n])
+        axs[1,1].imshow(data_y[:, n, :])
+        axs[2,1].imshow(data_y[n, :, :])
+        axs[0,2].imshow(data_z[:, :, n], clim=(-6e4, 6e4))
+        axs[1,2].imshow(data_z[:, n, :])
+        axs[2,2].imshow(data_z[n, :, :])
 
 
-    axs[0,0].set_title('$E_{x}$ XY plane')
-    axs[1,0].set_title('$E_{x}$ XZ plane')
-    axs[2,0].set_title('$E_{x}$ YZ plane')
-    axs[0,1].set_title('$E_{y}$ XY plane')
-    axs[1,1].set_title('$E_{y}$ XZ plane')
-    axs[2,1].set_title('$E_{y}$ YZ plane')
-    axs[0,2].set_title('$E_{z}$ XY plane')
-    axs[1,2].set_title('$E_{z}$ XZ plane')
-    axs[2,2].set_title('$E_{z}$ YZ plane')
-
+        axs[0,0].set_title('$E_{x}$ XY plane')
+        axs[1,0].set_title('$E_{x}$ XZ plane')
+        axs[2,0].set_title('$E_{x}$ YZ plane')
+        axs[0,1].set_title('$E_{y}$ XY plane')
+        axs[1,1].set_title('$E_{y}$ XZ plane')
+        axs[2,1].set_title('$E_{y}$ YZ plane')
+        axs[0,2].set_title('$E_{z}$ XY plane')
+        axs[1,2].set_title('$E_{z}$ XZ plane')
+        axs[2,2].set_title('$E_{z}$ YZ plane')
+        #plt.show()
+        plt.savefig('test{}.png'.format(i))
     #plt.quiver(data_x[::20, ::20, n], data_y[::20, ::20, n], scale=5e5)
-    plt.show()
+    #plt.show()
     pass
 
 
